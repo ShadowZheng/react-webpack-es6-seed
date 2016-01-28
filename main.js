af8367762e40aab3027712869024b59cec2a4a6d
Andy Zheng <azheng@objectivasoftware.com>
2016-01-28 17:06:32 +0800

    first commit.

---
+++ main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+require('./main.css');
+
+class HelloWorld extends React.Component {
+    render(){
+        return <h1>Hello World!</h1>;
+    }
+}
+
+ReactDOM.render(
+    <HelloWorld />,
+    document.getElementById('content')
+);
+
